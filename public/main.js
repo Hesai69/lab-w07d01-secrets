f048fb3377c05a50bd4c943d4f25dcd46c182c90
Qian Chen <oda_san@hotmail.com>
2017-01-23 14:16:53 -0800

    lies

--- public/main.js
+++ public/main.js
@@ -11,6 +11,28 @@ $('.like-btn').on('click', function(evt) {
   });
 });
 
+// example
+$('#create').on('click', function(evt) {
+  var newSecret = {
+    message: $('#secret_message').val()
+  }
+  $.post('/secrets', newSecret, function(res) {
+    console.log(res);
+    //$.get('/secrets/' + res._id);
+  })
+});
+// ajax delete method
+$('.remove-btn').on('click', function(evt) {
+  var id = $(evt.target).data().id;
+  $.ajax({
+    url: '/secrets/' + id,
+    method: 'DELETE'
+  }).then(function(res) {
+    console.log(res);
+    $(evt.target).parent().remove();
+  });
+});
+
 function render(secret) {
   var temp = $('template').html();
   var compile = Handlebars.compile(temp);
